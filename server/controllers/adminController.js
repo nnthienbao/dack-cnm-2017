93ad265e66fef5a7aaaab6ca58c90ecb96ff7f8f
Unknown <locphamduc96@gmail.com>
2018-01-09 01:59:59 +0700

    Quản lý hệ thống
    - xem danh sách tài khoản người dùng và số dư thực tế.
    - xem danh sách và thông tin của các giao dịch
    - xem danh sách các địa chỉ, số dư thực tế và số dư khả dụng

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,7 +1,10 @@
 const forEach = require('lodash').forEach;
 
 const User = require('../models/User');
+const TransactionLocal = require('../models/TransactionLocal');
 const Utils =  require('../common/Utils');
+
+const MAX_LIMIT = 100;
 
 module.exports.getStatistics = function (req, res) {
     let statistic = {
@@ -28,7 +31,8 @@ module.exports.getStatistics = function (req, res) {
 };
 
 module.exports.getInfoUserAccount = function (req, res) {
-    const limit = parseInt(req.query.limit);
+    let limit = parseInt(req.query.limit);
+    if(limit > MAX_LIMIT) limit = MAX_LIMIT;
     const page = parseInt(req.query.page);
 
     const option = {
@@ -56,3 +60,79 @@ module.exports.getInfoUserAccount = function (req, res) {
         return res.sendStatus(500);
     })
 };
+
+module.exports.getListTransaction = function (req, res) {
+    let limit = parseInt(req.query.limit);
+    if(limit > MAX_LIMIT) limit = MAX_LIMIT;
+    const page = parseInt(req.query.page);
+
+    const option = {
+        limit: limit,
+        skip: limit * page
+    };
+
+    let transactions = [];
+    TransactionLocal.find({}, {}, option).populate('_userId').then(listTrans => {
+        forEach(listTrans, trans => {
+            transactions.push({
+                username: trans._userId.username,
+                value: trans.value,
+                status: trans.status,
+                referencedOutputHash: trans.referencedOutputHash,
+                referencedOutputIndex: trans.referencedOutputIndex
+            })
+        });
+        return res.status(200).json(transactions);
+    }).catch(err => {
+        console.log(err);
+        return res.sendStatus(500);
+    })
+};
+
+module.exports.getListAddress = function (req, res) {
+    let limit = parseInt(req.query.limit);
+    if(limit > MAX_LIMIT) limit = MAX_LIMIT;
+    let page = parseInt(req.query.limit);
+
+    const option = {
+        limit: limit,
+        skip: limit * page
+    };
+
+    let listAddress = [];
+    User.find({}, {}, option).then(listUser => {
+        let promises = [];
+        forEach(listUser, user => {
+            promises.push(
+                Utils.getCoinByAddress(user.address).then(coin => {
+                    listAddress.push({
+                        address: user.address,
+                        username: user.username,
+                        coin: coin
+                    })
+                })
+            );
+            return Promise.all(promises).then(() => {
+                return res.status(200).json(listAddress);
+            });
+        })
+    }).catch(err => {
+        console.log(err);
+        return res.sendStatus(500);
+    })
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server/routers/api/admin.js
+++ server/routers/api/admin.js
@@ -7,8 +7,16 @@ Router.get('/statistic', (req, res) => {
     adminController.getStatistics(req, res);
 });
 
-Router.get('/get-user-account', (req, res) => {
+Router.get('/list/user-account', (req, res) => {
     adminController.getInfoUserAccount(req, res);
+});
+
+Router.get('/list/transaction', (req, res) => {
+    adminController.getListTransaction(req, res);
+});
+
+Router.get('/list/address', (req, res) => {
+    adminController.getListAddress(req, res);
 });
 
 module.exports = Router;
