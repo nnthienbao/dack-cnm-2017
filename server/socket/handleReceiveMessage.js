80f0626cb4d1f1cdf241a90ecb0514a11296b98c
Unknown <locphamduc96@gmail.com>
2018-01-10 02:34:40 +0700

    Update websocket api
    Lưu lại thông tin của giao dịch từ hệ thống khác tới người dùng trong hệ thống

--- server/socket/handleReceiveMessage.js
+++ server/socket/handleReceiveMessage.js
@@ -14,8 +14,6 @@ module.exports.executeListenedBlock = function(block) {
             //  Thay doi tien neu phat hien nguoi chuyen tu he thong
             if (transLocal) {
 
-                console.log("day la giao dich gui tien tu he thong");
-
                 const user = transLocal._userId;
                 user.lockedWallet -= transLocal.value;
                 user.realableWallet -= transLocal.value;
@@ -29,7 +27,7 @@ module.exports.executeListenedBlock = function(block) {
                 });
             }
             else {
-                forEach(trans.outputs, output => {
+                forEach(trans.outputs, (output, index) => {
 
                     let address = output.lockScript.split(" ")[1];
 
@@ -40,6 +38,19 @@ module.exports.executeListenedBlock = function(block) {
 
                             user.realableWallet = realableWallet;
                             user.save().catch(err => {
+                                console.log(err);
+                            })
+
+                            let newTransactionLocal = new TransactionLocal({
+                                value: output.value,
+                                receiverAddress: address,
+                                status: HOAN_THANH,
+                                referencedOutputHash: trans.hash,
+                                referencedOutputIndex: index,
+                                isLocal: false
+                            })
+
+                            newTransactionLocal.save().catch((err) => {
                                 console.log(err);
                             })
                         }
